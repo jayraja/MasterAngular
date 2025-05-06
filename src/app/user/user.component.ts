86b38b15612dff68e30deb75259af89c36f33e91
Jay Raja <jayraja15@hotmail.com>
2025-05-06 20:46:05 +0100

    first commit

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,36 @@
+import { Component, computed, EventEmitter, input, Input, Output } from '@angular/core';
+
+// type User = {
+//   id: string;
+//   name: string;
+//   avatar: string;
+// }
+
+interface User {
+  id: string;
+  name: string;
+  avatar: string;
+}
+
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+ @Input({required: true}) user!: User;
+
+ @Output() select = new EventEmitter<string>();
+   
+  onSelectUser(){
+    
+    this.select.emit(this.user.id);
+  }
+
+  get imagePath(){
+    return 'assets/users/' + this.user.avatar;
+  }
+
+}
